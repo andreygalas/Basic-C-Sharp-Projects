d67aed413d1900522051547fd3516e7ff94221a3
andreygalas <andreygalas2022@gmail.com>
2023-06-29 21:54:41 -0700

    commit

---
+++ CONSOLE APP PART SIX/CONSOLE APP PART SIX/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CONSOLE_APP_PART_SIX
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+         
+            {
+                List<string> stringList = new List<string> { "A", "B", "C", "D", "C" };
+                List<string> uniqueStrings = new List<string>();
+
+                foreach (string str in stringList)
+                {
+                    if (uniqueStrings.Contains(str))
+                    {
+                        Console.WriteLine($"{str} - this item is a duplicate");
+                    }
+                    else
+                    {
+                        uniqueStrings.Add(str);
+                        Console.WriteLine($"{str} - this item is unique");
+                    }
+                }
+
+                Console.ReadLine();
+            }
+        }
+
+    }
+}
