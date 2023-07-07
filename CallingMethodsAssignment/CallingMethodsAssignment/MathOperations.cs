3fa1f1b587bcd329e34c8409092ab5ee138aec80
andreygalas <andreygalas2022@gmail.com>
2023-07-07 01:17:58 -0700

    commit

---
+++ CallingMethodsAssignment/CallingMethodsAssignment/MathOperations.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class MathOperations
+{
+    // Method to perform addition on the received parameter
+    public int Add(int number)
+    {
+        // Add 5 to the received number
+        int result = number + 5;
+        return result;
+    }
+
+    // Method to perform multiplication on the received parameter
+    public int Multiply(int number)
+    {
+        // Multiply the received number by 10
+        int result = number * 10;
+        return result;
+    }
+
+    // Method to perform subtraction on the received parameter
+    public int Subtract(int number)
+    {
+        // Subtract 3 from the received number
+        int result = number - 3;
+        return result;
+    }
+}
---
+++ CallingMethodsAssignment/CallingMethodsAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    static void Main()
+    {
+        // Ask the user for a number to perform the math operations on
+        Console.Write("Enter a number: ");
+        int userInput = int.Parse(Console.ReadLine());
+
+        // Create an instance of the MathOperations class
+        MathOperations math = new MathOperations();
+
+        // Call each method in turn, passing the user input, and display the returned integers
+        int result1 = math.Add(userInput);
+        Console.WriteLine($"Result of addition: {result1}");
+
+        int result2 = math.Multiply(userInput);
+        Console.WriteLine($"Result of multiplication: {result2}");
+
+        int result3 = math.Subtract(userInput);
+        Console.WriteLine($"Result of subtraction: {result3}");
+
+        Console.ReadLine();
+    }
+}
