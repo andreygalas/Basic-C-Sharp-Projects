ce05f9946814926292d809d03f7a16ea3391bce3
andreygalas <andreygalas2022@gmail.com>
2023-07-12 11:35:39 -0700

    commit part3

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create the array of strings
+            string[] stringsArray = new string[5];
+
+            // Ask the user to input some text
+            Console.WriteLine("Enter some text:");
+            string userInput = Console.ReadLine();
+
+            // Iterate through the array and append the user's text to each string
+            for (int i = 0; i < stringsArray.Length; i++)
+            {
+                stringsArray[i] += userInput;
+            }
+
+            // Print each string in the array
+            Console.WriteLine("Updated array elements:");
+            for (int i = 0; i < stringsArray.Length; i++)
+            {
+                Console.WriteLine(stringsArray[i]);
+            }
+
+            // Loop using the "<" operator
+            Console.WriteLine("Loop using the \"<\" operator:");
+            for (int i = 0; i < stringsArray.Length; i++)
+            {
+                Console.WriteLine(stringsArray[i]);
+            }
+
+            // Loop using the "<=" operator
+            Console.WriteLine("Loop using the \"<=\" operator:");
+            for (int i = 0; i <= stringsArray.Length - 1; i++)
+            {
+                Console.WriteLine(stringsArray[i]);
+            }
+
+            // Add an infinite loop
+            while (true)
+            {
+                // Perform actions within the loop
+
+                // Break the loop conditionally
+                Console.WriteLine("Do you want to continue? (yes/no)");
+                string input = Console.ReadLine();
+
+                if (input.ToLower() != "yes")
+                {
+                    break;
+                }
+            }
+
+            // Wait for user input before closing the console window
+            Console.ReadLine();
+        }
+    }
+}
+   
