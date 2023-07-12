8d2ce83291c9abef5393e0552d251fa51f5b2cb9
andreygalas <andreygalas2022@gmail.com>
2023-07-12 11:53:08 -0700

    commit

---
+++ MainMethod/MainMethod/MathMethods.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodassign
+{
+    class MathMethods
+    {
+
+        //Method to add 10 to an integer and return an integer
+        public int Mathop(int number)
+        {
+            return number + 10;
+        }
+
+        public int Mathop(decimal number)
+        {
+            int newNum = Convert.ToInt32(number);
+            return newNum * 15;
+        }
+
+        //Method  to convert a string to an integer
+        public int Mathop(string number)
+        {
+            int newNum = Convert.ToInt32(number);
+            return newNum / 4;
+        }
+    }
+}
