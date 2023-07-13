7f471ef1ac9af66338f900c2ba2140287ee37002
andreygalas <andreygalas2022@gmail.com>
2023-07-12 18:16:32 -0700

    commit
    step 281

---
+++ MethodClass/MethodClass/Method.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass
+{
+    class Method
+    {
+        // Method that takes two integers as parameters and performs a math operation
+        public void mathOp(int firstNum, int secondNum)
+        {
+            firstNum += 5;
+            Console.WriteLine("The second input is: " + secondNum);
+        }
+    }
+}
+ 
---
+++ MethodClass/MethodClass/Program.cs
@@ -0,0 +1,28 @@
+﻿using MethodClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5;
+            int num2 = 15;
+
+            // Instantiate the Method class
+            Method myOp = new Method();
+
+            //Call the methOp in the class
+            myOp.mathOp(num1, num2);
+
+            //Call the methOp in the class
+            myOp.mathOp(secondNum: num2, firstNum: num1);
+            Console.ReadLine();
+        }
+    }
+}
