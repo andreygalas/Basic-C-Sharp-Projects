2f907a1a703b95837b788c2c99237b94f0412ff4
andreygalas <andreygalas2022@gmail.com>
2023-07-15 17:59:19 -0700

    commit

---
+++ MethodsandObjectsassign/MethodsandObjectsassign/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsandObjectsassign
+{
+    public class Person
+    {
+        //Person name definition 
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        //Method to display persons first and last name 
+        public void sayName()
+        {
+            Console.WriteLine("Name: " + firstName + " " + lastName);
+        }
+    }
+}
---
+++ MethodsandObjectsassign/MethodsandObjectsassign/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsandObjectsassign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate new Employee object and setting name 
+            Employee employee = new Employee() { firstName = "Sample", lastName = "Student" };
+
+            //Call inherited employee method to display the employee name 
+            employee.sayName();
+            Console.ReadLine();
+        }
+    }
+}
