89083827e3665ede47d38f41c388ea3633dbe57b
andreygalas <andreygalas2022@gmail.com>
2023-07-18 08:38:50 -0700

    commit

---
+++ PolymorphismAssignment/PolymorphismAssignment/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismAssignment
+{
+    class Employee : Person, IQuittable
+    {
+        // Implementation of the SayName() method
+        public override void SayName()
+        {
+            Console.WriteLine("Employee name is: {0} {1}", FirstName, LastName);
+        }
+
+        // Implementation of the Quit() 
+        public void Quit()
+        {
+            Console.WriteLine("This employee has quit");
+            FirstName = "";
+            LastName = "";
+        }
+    }
+}
---
+++ PolymorphismAssignment/PolymorphismAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using PolymorphismAssignment;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an object of type IQuittable using polymorphism
+            IQuittable quittable = new Employee();
+
+            // Call the Quit()
+            quittable.Quit();
+
+            // Keep the console window open
+            Console.ReadLine();
+        }
+    }
+}
