e1b3af988cdd7e7d7370b8aaaa7ad62f30843d80
andreygalas <andreygalas2022@gmail.com>
2023-07-22 19:04:10 -0700

    commit

---
+++ ConsttructorAssignment/ConsttructorAssignment/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsttructorAssignment
+{
+    class Book
+    {
+        //define properties for the Book class
+        public string Title { get; set; }
+        public string Author { get; set; }
+
+        //default constructor 
+        public Book()
+        {
+            this.Title = "Unknown";
+            this.Author = "Anonymous";
+        }
+    }
+}
+
---
+++ ConsttructorAssignment/ConsttructorAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsttructorAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create a const variable
+            const int myConstVariable = 42;
+            Console.WriteLine("The const variable value is: " + myConstVariable);
+
+            //create a variable using the keyword 'var'
+            var myVarVariable = "Hello, world!";
+            Console.WriteLine("The var variable value is: " + myVarVariable);
+
+            //chain two constructors together
+            Book book1 = new Book();
+
+            //Print the book details
+            Console.WriteLine("Book 1: " + book1.Title + " by " + book1.Author);
+
+            Console.ReadLine();
+        }
+    }
+}
+
