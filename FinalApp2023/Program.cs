c60479fe6d61420a0e42dc4d712cc2004d05d3a7
andreygalas <andreygalas2022@gmail.com>
2023-08-07 20:47:05 -0700

    commit

--- FinalApp2023/Program.cs
+++ FinalApp2023/Program.cs
@@ -1,58 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-
-
+﻿using FinalChallangeAssignment;
 using System;
-using Microsoft.EntityFrameworkCore;
+using FinalChallengeAssignment;
 
-class Program
+namespace FinalChallengeAssignment
 {
-    static void Main()
+    class Program
     {
-        // Ensure the database is created and apply any pending migrations.
-        using (var context = new StudentDbContext())
-        {
-           
-        }
 
-        // Add a student to the database.
-        using (var context = new StudentDbContext())
+
+        static void Main(string[] args)
         {
-            var student = new Student
+            using (var ctx = new SchoolContext())
             {
-                Name = "John Doe",
-                Age = 20
-            };
+                var student = new Student() { StudentName = "Andrey" };
 
-            context.Students.Add(student);
-            context.SaveChanges();
+                ctx.Students.Add(student);
+                ctx.SaveChanges();
+            }
+            Console.WriteLine("Demo completed.");
+            Console.ReadLine();
         }
-
-        Console.WriteLine("Student added successfully!");
     }
-}
-
-
-
-public class Student
-{
-    [Key]
-    public int Id { get; set; }
-    
-    public string Name { get; set; }
-
-    public int Age { get; set; }
-}
-
-public class StudentDbContext : DbContext
-{
-    public DbSet<Student> Students { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-    optionsBuilder.UseSqlServer("Server=ANDREY\\SQLEXPRESS;Database=aspnet-WebApplication3-53bc9b9d-9d6a-45d4-8429-2a2761773502;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=true", options => options.EnableRetryOnFailure());
-
-    }
-
 }
